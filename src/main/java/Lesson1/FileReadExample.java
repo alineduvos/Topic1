9d740085d47fd2586679d705507711c14dccbefb
aysham <alineduvos@gmail.com>
2025-01-13 11:50:05 +0200

    Reading a Textfile using FileInputStream

---
+++ src/main/java/Lesson1/FileReadExample.java
@@ -0,0 +1,34 @@
+ 
+package Lesson1;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+ 
+public class FileReadExample {
+    
+    public static void main(String[] args) {
+        
+        FileInputStream fis = null;
+        
+        try {
+            String filePath = "example.txt";
+            fis = new FileInputStream(filePath);
+            
+            int byteData; // to case the character later for display
+            System.out.println("File Contents : ");
+            
+            while((byteData = fis.read()) != -1){
+                System.out.print((char)byteData);
+            }
+            
+        } catch (IOException ex) {
+            System.out.println("Error : "+ex.getMessage());
+        }  
+        
+    }
+    
+}
